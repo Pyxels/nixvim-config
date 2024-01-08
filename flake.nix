64a6c257b0b6888e71b58fd793561d878a7ce0c4
Pyxels <39232833+Pyxels@users.noreply.github.com>
2024-01-08 16:54:27 +0100

    chore(flake): make nixvim follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,9 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     nixvim.url = "github:nix-community/nixvim";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
