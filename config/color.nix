38fdc071d76cf5169ec2574179215359e2ada665
Pyxels <39232833+Pyxels@users.noreply.github.com>
2024-04-12 07:33:13 +0200

    refactor(color): use nixpkgs gruvbox-baby

--- config/color.nix
+++ config/color.nix
@@ -1,14 +1,4 @@
-{pkgs, ...}: let
-  gruvbox-baby = pkgs.vimUtils.buildVimPlugin {
-    name = "gruvbox-baby";
-    src = pkgs.fetchFromGitHub {
-      owner = "luisiacc";
-      repo = "gruvbox-baby";
-      rev = "bd52e62d8134647090108189e69c8b3cd18bdbbf";
-      hash = "sha256-UhOYkVInyQrPy970NMKfFhq3JZA2zuWQdyDOfv9glp8=";
-    };
-  };
-in {
-  extraPlugins = [gruvbox-baby];
+{pkgs, ...}: {
+  extraPlugins = with pkgs.vimPlugins; [gruvbox-baby];
   colorscheme = "gruvbox-baby";
 }
