11850b062f6d16c869b0464dca7cbfa35eb99ee8
Pyxels <39232833+Pyxels@users.noreply.github.com>
2024-07-08 13:11:27 +0200

    fix(color): add highlighting colors for search an replace

--- config/color.nix
+++ config/color.nix
@@ -1,4 +1,14 @@
 {pkgs, ...}: {
   extraPlugins = with pkgs.vimPlugins; [gruvbox-baby];
+
   colorscheme = "gruvbox-baby";
+
+  # make s/x/y/gc highlighting work, see https://github.com/luisiacc/gruvbox-baby/issues/70#issuecomment-2205861831
+  globals.gruvbox_baby_highlights = {
+    IncSearch = {
+      bg = "#ff9e64";
+      fg = "White";
+    };
+    CurSearch = {link = "IncSearch";};
+  };
 }
