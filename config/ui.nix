35d269a82653e3267c32af27c3051738edd8d4ca
Pyxels <39232833+Pyxels@users.noreply.github.com>
2024-11-08 09:38:27 +0100

    chore(inputs): update all inputs

--- config/ui.nix
+++ config/ui.nix
@@ -29,14 +29,16 @@
 
     noice = {
       enable = true;
-      lsp.override = {
-        "cmp.entry.get_documentation" = true;
-        "vim.lsp.util.convert_input_to_markdown_lines" = true;
-        "vim.lsp.util.stylize_markdown" = true;
-      };
-      presets = {
-        bottom_search = true;
-        command_palette = true;
+      settings = {
+        lsp.override = {
+          "cmp.entry.get_documentation" = true;
+          "vim.lsp.util.convert_input_to_markdown_lines" = true;
+          "vim.lsp.util.stylize_markdown" = true;
+        };
+        presets = {
+          bottom_search = true;
+          command_palette = true;
+        };
       };
     };
   };
