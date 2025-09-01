d9a902528549c93b8c80137e337e862674b1e696
Pyxels <39232833+Pyxels@users.noreply.github.com>
2025-09-01 14:09:48 +0200

    revert(lsp): go back to base prettier
    prettierd does not respect project config

--- config/lsp.nix
+++ config/lsp.nix
@@ -52,7 +52,7 @@
 
       sources = {
         formatting = {
-          prettierd = {
+          prettier = {
             enable = true;
             disableTsServerFormatter = true;
           };
