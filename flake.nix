70f8017235bf1be927a8c2f673752436a61e3f6c
Pyxels <39232833+Pyxels@users.noreply.github.com>
2025-09-22 15:03:39 +0200

    chore(flake): update inputs

--- flake.nix
+++ flake.nix
@@ -24,61 +24,63 @@
   } @ inputs: let
     config = import ./config; # import the module directly
   in
-    flake-parts.lib.mkFlake {inherit inputs;} ({moduleWithSystem, ...}: {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
+    flake-parts.lib.mkFlake {inherit inputs;} (
+      {moduleWithSystem, ...}: {
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ];
 
-      perSystem = {
-        pkgs,
-        system,
-        self',
-        ...
-      }: let
-        nixvim-config = nixvim.legacyPackages.${system}.makeNixvimWithModule {
-          inherit pkgs;
-          module = config;
-        };
-      in {
-        checks = {
-          # Run `nix flake check .` to verify that the config is not broken
-          nixvim = nixvim.lib.${system}.check.mkTestDerivationFromNvim {
-            nvim = nixvim-config;
-            name = "A nixvim configuration";
+        perSystem = {
+          pkgs,
+          system,
+          self',
+          ...
+        }: let
+          nixvim-config = nixvim.legacyPackages.${system}.makeNixvimWithModule {
+            inherit pkgs;
+            module = config;
+          };
+        in {
+          checks = {
+            # Run `nix flake check .` to verify that the config is not broken
+            nixvim = nixvim.lib.${system}.check.mkTestDerivationFromNvim {
+              nvim = nixvim-config;
+              name = "A nixvim configuration";
+            };
+
+            git-hooks = import ./git-hooks.nix {inherit git-hooks system;};
           };
 
-          git-hooks = import ./git-hooks.nix {inherit git-hooks system;};
+          devShells.default = pkgs.mkShell {
+            inherit (self'.checks.git-hooks) shellHook;
+            buildInputs = self'.checks.git-hooks.enabledPackages;
+          };
+
+          packages = {
+            inherit nixvim-config;
+            # Lets you run `nix run .` to start nixvim
+            default = nixvim-config;
+          };
         };
 
-        devShells.default = pkgs.mkShell {
-          inherit (self'.checks.git-hooks) shellHook;
-          buildInputs = self'.checks.git-hooks.enabledPackages;
+        flake = {
+          homeModules.default = moduleWithSystem (
+            perSystem: _: {
+              imports = [./home-manager-module.nix];
+              nixvim-config.package = perSystem.config.packages.default;
+            }
+          );
+
+          nixosModules.default = moduleWithSystem (
+            perSystem: _: {
+              imports = [./nixos-module.nix];
+              nixvim-config.package = perSystem.config.packages.default;
+            }
+          );
         };
-
-        packages = {
-          inherit nixvim-config;
-          # Lets you run `nix run .` to start nixvim
-          default = nixvim-config;
-        };
-      };
-
-      flake = {
-        homeModules.default = moduleWithSystem (
-          {config} @ perSystem: _: {
-            imports = [./home-manager-module.nix];
-            nixvim-config.package = perSystem.config.packages.default;
-          }
-        );
-
-        nixosModules.default = moduleWithSystem (
-          {config} @ perSystem: _: {
-            imports = [./nixos-module.nix];
-            nixvim-config.package = perSystem.config.packages.default;
-          }
-        );
-      };
-    });
+      }
+    );
 }
